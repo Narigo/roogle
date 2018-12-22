34f4ff3e11aa144e611de3d65d5045789cb09026
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-12-22 03:46:42 +0100

    Add parameter how many sentences to take and take them from random positions
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- index.js
+++ index.js
@@ -1,39 +1,94 @@
 #!/usr/bin/env node
 const stream = require("stream");
 
+const maximumNumberOfSentences = process.argv[2] || 5;
+
 // more or less google queries
-streamToGoogleQuery(process.stdin, 5, 50);
+streamToGoogleQuery(process.stdin, maximumNumberOfSentences, 50);
 
 function streamToGoogleQuery(inputStream, sentences, minimumLength) {
   process.stdout.on("error", () => {});
   return inputStream
     .pipe(tokenizeSentences())
-    .pipe(filterRandomly(sentences, minimumLength))
+    .pipe(mergeAllSentences())
+    .pipe(filterByMinimumLength(50))
+    .pipe(splitArrayIntoParts(sentences))
+    .pipe(selectOneRandomly())
+    .pipe(toCommands())
     .pipe(process.stdout);
 }
 
+//`open https://www.google.de/search?q=${encodeURIComponent(maybeSentence)}\n`);
 function tokenizeSentences() {
   return new stream.Transform({
     objectMode: true,
     transform(chunk, encoding, cb) {
+      const possibleSentences = [];
       chunk.toString(encoding).split(/[:.!?\n]/).forEach(sentence => {
         const maybeSentence = sentence.replace(/[^a-zäöüß\- ]/ig, "").trim();
         if (maybeSentence !== "") {
-          this.push(`open https://www.google.de/search?q=${encodeURIComponent(maybeSentence)}\n`);
+          possibleSentences.push(maybeSentence);
         }
       });
+      this.push(possibleSentences);
       cb();
     }
   });
 }
 
-function filterRandomly(amountOfSentences, minimumLength) {
-  let counter = 0;
+function mergeAllSentences() {
+  let chunks = [];
   return new stream.Transform({
+    objectMode: true,
     transform(chunk, encoding, cb) {
-      if (++counter % amountOfSentences === 0 && chunk.length > minimumLength) {
-        this.push(chunk);
+      chunks = chunks.concat(chunk);
+      cb();
+    },
+    flush(cb) {
+      this.push(chunks);
+      cb();
+    }
+  });
+}
+
+function filterByMinimumLength(length) {
+  return new stream.Transform({
+    objectMode: true,
+    transform(chunk, encoding, cb) {
+      this.push(chunk.filter(str => length <= str.length));
+      cb();
+    }
+  });
+}
+
+function splitArrayIntoParts(amountOfChunks) {
+  return new stream.Transform({
+    objectMode: true,
+    transform(chunk, encoding, cb) {
+      const splitEvery = Math.floor(chunk.length / amountOfChunks);
+      for (let i = 0; (i * splitEvery) < chunk.length; i++) {
+        this.push(chunk.slice(i * splitEvery, (i + 1) * splitEvery));
       }
+      cb();
+    }
+  });
+}
+
+function selectOneRandomly() {
+  return new stream.Transform({
+    objectMode: true,
+    transform(chunk, encoding, cb) {
+      this.push(chunk[Math.floor(Math.random() * chunk.length)]);
+      cb();
+    }
+  });
+}
+
+function toCommands() {
+  return new stream.Transform({
+    objectMode: true,
+    transform(chunk, encoding, cb) {
+      this.push(`open https://www.google.com/search?q=${encodeURIComponent(chunk)}\n`);
       cb();
     }
   });
