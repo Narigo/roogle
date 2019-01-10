9af9bc8930cc28ed0d9654c8c84580b9063eed02
Joern Bernhardt <joern.bernhardt@googlemail.com>
2019-01-10 03:27:43 +0100

    Add more tests
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- test.js
+++ test.js
@@ -17,14 +17,18 @@ async function run() {
   return runAllTests();
 
   async function runAllTests() {
-    await test("five-sentences-text");
-    await test("short-text");
-    await test("text-with-filtered-sentences");
-    await testIncludedButNotAll("twelve-sentences-text");
+    await test("five-sentences-text", {});
+    await test("short-text", {});
+    await test("text-with-filtered-sentences", {});
+    await test("text-with-filtered-sentences", {});
+    await test("twelve-sentences-text", { maximumNumberOfSentences: 12 });
+    await test("filter-by-chars", { minimumLengthOfSentence: 53 });
+    await test("raw-text-with-filtered-sentences", { prefix: "", raw: true, suffix: "" });
+    await testIncludedButNotAll("twelve-sentences-text", {});
   }
 
-  async function testIncludedButNotAll(name) {
-    return assertAgainstOutput(name, (expectedOutput, testOutput) => {
+  async function testIncludedButNotAll(name, options) {
+    return assertAgainstOutput(name, options, (expectedOutput, testOutput) => {
       const possibilities = expectedOutput.split(/\n/).filter(line => line !== "");
       const results = testOutput.split(/\n/).filter(line => line !== "");
       for (let line of results) {
@@ -39,8 +43,8 @@ async function run() {
     });
   }
 
-  async function test(name) {
-    return assertAgainstOutput(name, (expectedOutput, testOutput) =>
+  async function test(name, options) {
+    return assertAgainstOutput(name, options, (expectedOutput, testOutput) =>
       assert(
         testOutput === expectedOutput,
         `output does not match expectation in test ${name}:\n${testOutput}\n!==\n${expectedOutput}`
@@ -48,13 +52,14 @@ async function run() {
     );
   }
 
-  async function assertAgainstOutput(name, assertions) {
+  async function assertAgainstOutput(name, options, assertions) {
     const expectedOutput = await readFile(`__test__/${name}.expected.txt`, "utf8");
     const tempTestOutputFile = `test-output-${name}.tmp.txt`;
     await new Promise((resolve, reject) => {
       const stream = streamToGoogleQuery({
         inputStream: fs.createReadStream(`__test__/${name}.txt`),
-        outputStream: fs.createWriteStream(tempTestOutputFile)
+        outputStream: fs.createWriteStream(tempTestOutputFile),
+        ...options
       });
       stream.on("error", reject);
       stream.on("close", resolve);
